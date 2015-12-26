7c9b313539ac171d64fa0b5ccd3d6f348bc986f2
Matt Wrock <matt@mattwrock.com>
2015-12-25 17:07:39 -0800

    fixes #9 removing extra param of fetch_role

--- lib/kitchen/provisioner/nodes_version.rb
+++ lib/kitchen/provisioner/nodes_version.rb
@@ -19,6 +19,6 @@
 module Kitchen
   # Version string for Nodes Kitchen driver
   module Provisioner
-    NODES_VERSION = '0.6.1'
+    NODES_VERSION = '0.6.2'
   end
 end
--- lib/kitchen/provisioner/run_list_expansion_from_kitchen.rb
+++ lib/kitchen/provisioner/run_list_expansion_from_kitchen.rb
@@ -22,8 +22,13 @@ module Kitchen
   module Provisioner
     # fetches roles from kitchen roles directory
     class RunListExpansionFromKitchen < ::Chef::RunList::RunListExpansion
-      def fetch_role(name, included_by, role_dir)
-        role_file = File.join(role_dir, "#{name}.json")
+      def initialize(environment, run_list_items, source = nil, role_dir = nil)
+        @role_dir = role_dir
+        super(environment, run_list_items, source)
+      end
+
+      def fetch_role(name, included_by)
+        role_file = File.join(@role_dir, "#{name}.json")
         Chef::Role.json_create(JSON.parse(File.read(role_file)))
       rescue Chef::Exceptions::RoleNotFound
         role_not_found(name, included_by)
